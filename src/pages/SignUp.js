d33120f03a7fb70a0252499e7fa4256d1008399b
Shuri-Honda-1101 <knh065146@gmail.com>
2021-04-21 20:40:40 +0900

    [update]Cut out ModalCropper as a component

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,49 +5,20 @@ import Logo from "../img/logo.png";
 import iconDefault from "../img/icon_default.png";
 import iconMask from "../img/icon-mask.png";
 import Button from "@material-ui/core/Button";
-import Slider from "@material-ui/core/Slider";
-import Cropper from "react-easy-crop";
-import getCroppedImg from "../cropImage";
-import CloseIcon from "@material-ui/icons/Close";
+import { ModalCropper } from "./ModalCropper";
 
 export const SignUp = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [src, setSrc] = useState(null);
-  const [crop, setCrop] = useState({ x: 0, y: 0 });
-  const [zoom, setZoom] = useState(1);
-  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
   const [croppedImage, setCroppedImage] = useState(null);
   const [selectImageValue, setSelectImageValue] = useState("");
   const [croppedImageUrl, setCroppedImageUrl] = useState(iconDefault);
 
-  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    setCroppedAreaPixels(croppedAreaPixels);
-  }, []);
-
-  const showCroppedImage = useCallback(async () => {
-    try {
-      const croppedImage = await getCroppedImg(src, croppedAreaPixels);
-      console.log("donee", { croppedImage });
-      setCroppedImageUrl(croppedImage);
-      let blobImage = await fetch(croppedImage).then((r) => r.blob());
-      setCroppedImage(blobImage);
-      setSrc(null);
-      setSelectImageValue("");
-    } catch (e) {
-      console.error(e);
-    }
-  }, [src, croppedAreaPixels]);
-
   const onChangeFile = (e) => {
     setSrc(URL.createObjectURL(e.target.files[0]));
     console.log(e.target.files[0]);
-  };
-
-  const onClickClose = () => {
-    setSrc(null);
-    setSelectImageValue("");
   };
 
   const handleSubmit = (e) => {
@@ -103,41 +74,13 @@ export const SignUp = ({ history }) => {
               </IconUp>
             </div>
             {src && (
-              <ModalCropperWrap>
-                <ModalCropper>
-                  <CloseIcon onClick={onClickClose} />
-                  <div className="cropper-wrap">
-                    <Cropper
-                      image={src}
-                      crop={crop}
-                      zoom={zoom}
-                      aspect={1}
-                      cropShape="round"
-                      onCropChange={setCrop}
-                      onCropComplete={onCropComplete}
-                      onZoomChange={setZoom}
-                    />
-                  </div>
-                  <div className="slider-wrap">
-                    <Slider
-                      value={zoom}
-                      min={1}
-                      max={3}
-                      step={0.1}
-                      aria-labelledby="Zoom"
-                      onChange={(e, zoom) => setZoom(zoom)}
-                      classes={{ root: "slider" }}
-                    />
-                  </div>
-                  <Button
-                    className="cropper-ok"
-                    variant="contained"
-                    onClick={showCroppedImage}
-                  >
-                    OK
-                  </Button>
-                </ModalCropper>
-              </ModalCropperWrap>
+              <ModalCropper
+                setSrc={setSrc}
+                setSelectImageValue={setSelectImageValue}
+                src={src}
+                setCroppedImageUrl={setCroppedImageUrl}
+                setCroppedImage={setCroppedImage}
+              />
             )}
             <div className="input-wrap">
               <input
@@ -310,77 +253,5 @@ const IconUp = styled.label`
     border-radius: 50%;
     height: 11.5vw; //22rem;
     width: 11.5vw; //22rem;
-  }
-`;
-
-const ModalCropperWrap = styled.div`
-  z-index: 4;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ModalCropper = styled.div`
-  z-index: 5;
-  height: 45.6vw; //87.5rem;
-  width: 40.2vw; //77.2rem;
-  border-radius: 3.4vw; //6.5rem;
-  background-color: #fff;
-
-  .MuiSvgIcon-root {
-    font-size: 2.6vw; //5rem;
-    margin: 2.1vw 2.86vw 0 0; //4rem 5.5rem 0 0;
-    float: right; 
-    cursor: pointer;
-    color: #EF4565;
-  }
-
-  .cropper-wrap {
-    position: relative;
-    height: 25.62vw; //49.2rem;
-    width: 25.62vw; //49.2rem;
-    margin: 6.25vw auto 2.38vw; //12rem auto 4.57rem;
-    .reactEasyCrop_Container {
-      height: 100%;
-      width: 100%;
-      object-fit: contain;
-      img {
-        height: 26vw; //50rem;
-      }
-      div {
-        height: 100%;
-        width: 100%;
-      }
-    }
-  }
-
-  .slider-wrap {
-    width: 50%;
-    margin: 0 auto;
-  }
-
-  .cropper-ok {
-    display: block;
-    margin: 2.4vw auto 0; //4.6rem auto 0;
-    background-color: #ef4565;
-      color: #fff;
-      font-size: 1.8vw; //3.5rem;
-      border-radius: 0.62vw; //1.2rem;
-      width: 11.4vw; //21.9rem;
-      height: 3.9vw; //7.5rem;
-      font-family: "ヒラギノ丸ゴ ProN";
-      font-weight: normal;
-      line-height: 3.1vw; //6rem;
-      :hover {
-        background-color: #dc004e;
-      }
-    }
   }
 `;
